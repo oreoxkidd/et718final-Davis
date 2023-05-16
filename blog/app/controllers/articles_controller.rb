3dd6aef1e441e791596ebdef84fb74a50e31cfc3
EC2 Default User <ec2-user@ip-10-16-0-37.ec2.internal>
2023-05-16 17:52:31 +0000

    end of final

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,13 +1,20 @@
 class ArticlesController < ApplicationController
-    def show
-        @article =Article.find(params[:id])
+    
+    def index
+    @articles = Article.all
     end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    # snippet for brevity
     
     def new
     end
     
     def create
-        @article = Article.new(params.require(:article).permit(:title, :text))
+        @article = Article.new(article.params)
  
         @article.save
         redirect_to @article
